9f2d10cef5ddc07e29847600184ff3c380afded6
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-05-12 14:18:42 -0700

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "bulma/css/bulma.css";
+import "./styles.scss";
+const reducer = () => {
+  return { title: "Hello world! I'm in the Redux store!" };
+};
+const store = createStore(reducer);
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
